2e0c41799d80dc6ec875f15149098f57ee67bab7
Nicolas Mayorga <namayorgav@gmail.com>
2023-01-15 00:29:59 -0500

    feat: Data fetched correctly with useEffect

--- Tours2.0/src/App.jsx
+++ Tours2.0/src/App.jsx
@@ -15,7 +15,6 @@ const App = () => {
 
     try {
       const response = await fetch(API_URL)
-
       const toursData = await response.json()
       setTours(toursData)
       // setLoading(false)
