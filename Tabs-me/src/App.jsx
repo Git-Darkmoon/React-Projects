10dd0ad6894bd6b9c29bd5cf633111215a0e9ceb
Nicolas Mayorga <namayorgav@gmail.com>
2023-03-03 23:44:47 -0500

    chore: Finish the list-style of the jobs. Project Finished

--- Tabs-me/src/App.jsx
+++ Tabs-me/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react"
 import Loader from "./Loader"
 import styles from "./styles/App.module.css"
-
+import { BsCodeSlash } from "react-icons/bs"
 const URL = "https://course-api.com/react-tabs-project"
 
 function App() {
   const [loading, setLoading] = useState(true)
   const [jobs, setJobs] = useState([])
-
+  const [jobIndex, setJobIndex] = useState(0)
   const fetchJobs = async () => {
     const response = await fetch(URL)
     const jobsData = await response.json()
@@ -24,7 +24,7 @@ function App() {
     return <Loader />
   }
 
-  const { id, order, title, dates, duties, company } = jobs
+  const { id, order, title, dates, duties, company } = jobs[jobIndex]
 
   return (
     <>
@@ -33,20 +33,34 @@ function App() {
         <div className={styles.underline}></div>
       </header>
 
-      <main>
+      <main className={styles.container}>
         {/* btn container */}
-        <section className={styles.btnContainer}></section>
-        {/* Jobs container */}
-        <section className={styles.jobsContainer}>
-          {jobs.map(() => {
+        <section className={styles.btnContainer}>
+          {jobs.reverse().map((job, index) => {
             return (
-              <article key={id}>
-                <h3>{title}</h3>
-                <h4>{company}</h4>
-                <p>{dates}</p>
-              </article>
+              <button key={job.id} onClick={() => setJobIndex(job.order - 1)}>
+                {job.company}
+              </button>
             )
           })}
+        </section>
+        {/* Jobs container */}
+        <section className={styles.jobsContainer}>
+          <article key={id}>
+            <h3 className={styles.jobTitle}>{title}</h3>
+            <span className={styles.company}>{company}</span>
+            <p className={styles.jobDate}>{dates}</p>
+            <ul>
+              {duties.map((duty, index) => {
+                return (
+                  <li key={index} className={styles.duty}>
+                    <BsCodeSlash className={styles.jobIcon} />
+                    {duty}
+                  </li>
+                )
+              })}
+            </ul>
+          </article>
         </section>
       </main>
     </>
