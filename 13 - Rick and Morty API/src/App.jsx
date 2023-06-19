eacc63a4d2c5a8ec9b3b98ac8efde533f2762847
Nicolas Mayorga <namayorgav@gmail.com>
2023-06-18 20:30:12 -0500

    Hero and fetch done, some perfomance issues with the characters
    Responsive Design working

--- 13 - Rick and Morty API/src/App.jsx
+++ 13 - Rick and Morty API/src/App.jsx
@@ -1,13 +1,17 @@
+import Characters from "./Characters"
 import Hero from "./Hero"
 import title from "./assets/title.png"
 
 function App() {
   return (
     <>
-      <header>
-        <img src={title} alt="" />
-      </header>
-      <Hero />
+      <main className="principal">
+        <header>
+          <img src={title} alt="" />
+        </header>
+        <Hero />
+      </main>
+      <Characters />
     </>
   )
 }
