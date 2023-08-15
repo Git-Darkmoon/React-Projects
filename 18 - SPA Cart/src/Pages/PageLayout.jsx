1ece684132d8398269d5068d1dcb15f729a3e2cf
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-14 21:44:37 -0500

    feat: shared layout w/ outlet and loading state.

--- 18 - SPA Cart/src/Pages/PageLayout.jsx
+++ 18 - SPA Cart/src/Pages/PageLayout.jsx
@@ -1,4 +1,25 @@
+import { Outlet, useNavigation } from "react-router-dom"
+import Navbar from "../Components/Navbar"
+import Footer from "../Components/Footer"
+
 function PageLayout() {
-  return <div>PageLayout</div>
+  const navigation = useNavigation()
+  const isLoading = navigation.state === "loading"
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        {isLoading ? (
+          <div className="loading-container">
+            <div className="loading-spinner"></div>
+          </div>
+        ) : (
+          <Outlet />
+        )}
+      </div>
+      <Footer />
+    </>
+  )
 }
 export default PageLayout
