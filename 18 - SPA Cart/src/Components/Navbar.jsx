31b5ea3c5635846295ef41cba370a32af654f91e
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-14 21:43:00 -0500

    feat: implements NavLink from react-router.

--- 18 - SPA Cart/src/Components/Navbar.jsx
+++ 18 - SPA Cart/src/Components/Navbar.jsx
@@ -1,17 +1,23 @@
+import { NavLink } from "react-router-dom"
+
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
         <h1>iTours</h1>
-        <ul className="linksList">
-          <li>
-            <a className="links" href=""></a>
-          </li>
-          <li>
-            <a className="links" href=""></a>
-          </li>
-        </ul>
       </div>
+      <ul className="linksList">
+        <li>
+          <NavLink to={"/"} className={"links"}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/about"} className={"links"}>
+            About
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   )
 }
