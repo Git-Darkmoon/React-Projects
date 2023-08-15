f11219c80c700e5a2e3ca3f6704053dc9a54fa2c
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-14 21:38:39 -0500

    route: Added the about route and import.

--- 18 - SPA Cart/src/App.jsx
+++ 18 - SPA Cart/src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import PageLayout from "./Pages/PageLayout"
 import Tours from "./Pages/Tours"
 import TourInfo from "./Pages/TourInfo"
+import About from "./Pages/About"
 import ErrorPage from "./Pages/ErrorPage"
 
 const router = createBrowserRouter([
@@ -18,6 +19,11 @@ const router = createBrowserRouter([
       {
         path: "tourInfo/:id",
         element: <TourInfo />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "about",
+        element: <About />,
         errorElement: <ErrorPage />,
       },
     ],
