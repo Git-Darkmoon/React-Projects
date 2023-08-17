125fd7dd05c8b63ad4667e9c5a6317f641b04f65
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-16 20:17:16 -0500

    add: QueryClientProvider.

--- 18 - SPA Cart/src/main.jsx
+++ 18 - SPA Cart/src/main.jsx
@@ -2,9 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.scss"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>
 )
