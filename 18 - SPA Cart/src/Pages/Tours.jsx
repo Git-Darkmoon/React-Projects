7bdbb31cde228ad9c3769fab438699ebf82ece4e
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-16 20:13:26 -0500

    fetch: useQuery for API w/ conditional rendering.

--- 18 - SPA Cart/src/Pages/Tours.jsx
+++ 18 - SPA Cart/src/Pages/Tours.jsx
@@ -1,4 +1,40 @@
+import { useQuery } from "@tanstack/react-query"
+import TourInfo from "./TourCard"
+
+const API_URL = "https://www.course-api.com/react-tours-project"
+
 function Tours() {
-  return <div>Tours</div>
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["tours"],
+    queryFn: async () => {
+      const response = await fetch(API_URL)
+      const tours = response.json()
+      return tours
+    },
+  })
+
+  if (isLoading) {
+    return (
+      <section className="tours-container">
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
+  if (isError) {
+    return (
+      <section className="tours-container">
+        <h1>Something Went wrong...</h1>
+      </section>
+    )
+  }
+
+  return (
+    <section className="tours-container">
+      {data.map((eachTour) => {
+        return <TourInfo key={eachTour.id} {...eachTour} />
+      })}
+    </section>
+  )
 }
 export default Tours
