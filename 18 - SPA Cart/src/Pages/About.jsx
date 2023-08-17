8b0668fe218a434127fffff510533acb79be02dc
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-16 20:14:50 -0500

    add: content.

--- 18 - SPA Cart/src/Pages/About.jsx
+++ 18 - SPA Cart/src/Pages/About.jsx
@@ -1,4 +1,47 @@
 function About() {
-  return <div>About</div>
+  return (
+    <section className="about">
+      <p>
+        Greetings and welcome to our immersive world of exploration and
+        adventure! I&apos;m thrilled to have you here on our digital doorstep.
+        As both a frontend developer expert and a UX Designer, I&apos;m
+        committed to crafting not just tours, but unforgettable journeys that
+        resonate with your wanderlust.
+      </p>
+      <p>
+        At iTours, we&apos;ve mastered the art of transforming your dreams into
+        reality. Our passion for travel, paired with innovative design, ensures
+        that every step you take with us is a seamless and delightful
+        experience. Whether you&apos;re an avid traveler or a curious explorer,
+        we&apos;ve curated a selection of awe-inspiring tours that span the
+        globe.
+      </p>
+
+      <p>
+        Each tour we offer has its own narrative, a tale that unfolds as you
+        traverse new landscapes and cultures. Our goal is to not only provide
+        you with a vacation but to let you become a part of the story, leaving
+        with memories that last a lifetime.
+      </p>
+      <p>
+        Join us on a voyage that transcends the ordinary. Let us be your guides,
+        your companions, and your source of inspiration as you uncover the
+        hidden gems and iconic landmarks that this world has to offer. Embark on
+        a journey that&apos;s meticulously designed, thoughtfully marketed, and
+        endlessly enchanting.
+      </p>
+      <p>
+        Thank you for choosing iTours for your global adventures. Here&apos;s to
+        exploring the world together!
+      </p>
+      <p className="last-p">
+        Warmly,
+        <br />
+        Nicolas
+        <br />
+        Frontend Dev and UX Designer
+      </p>
+    </section>
+  )
 }
 export default About
