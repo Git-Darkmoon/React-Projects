5e7dc492a0cae1202d73a5ae1789b42bbc95b050
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-22 18:01:08 -0500

    feat:AppProvider component for using globalContext

--- 18 - SPA Cart/src/main.jsx
+++ 18 - SPA Cart/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.scss"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { AppProvider } from "./context.jsx"
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <AppProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </AppProvider>
   </QueryClientProvider>
 )
